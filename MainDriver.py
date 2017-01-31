43a9e88c53646dbf045ccc7628fffab14d4b7183
Gopal Menon <gopal.mnn@gmail.com>
2017-01-30 22:05:02 -0700

    Finished n grams

--- MainDriver.py
+++ MainDriver.py
@@ -11,15 +11,77 @@ TWO_GRAM = 2
 THREE_GRAM = 3
 
 """Construct character 2-grams for all documents"""
-d1_2_grams = NGrams.get_character_n_grams(FileIo.get_text_file_contents(FILE_D1), TWO_GRAM)
-d2_2_grams = NGrams.get_character_n_grams(FileIo.get_text_file_contents(FILE_D2), TWO_GRAM)
-d3_2_grams = NGrams.get_character_n_grams(FileIo.get_text_file_contents(FILE_D3), TWO_GRAM)
-d4_2_grams = NGrams.get_character_n_grams(FileIo.get_text_file_contents(FILE_D4), TWO_GRAM)
+d1_char_2_grams = NGrams.get_character_n_grams(FileIo.get_text_file_contents(FILE_D1), TWO_GRAM)
+d1_char_2_gram_set = set()
+for n_gram in d1_char_2_grams:
+    d1_char_2_gram_set.add(n_gram)
+print("D1.txt has " + str(len(d1_char_2_gram_set)) + " character 2-grams")
+
+d2_char_2_grams = NGrams.get_character_n_grams(FileIo.get_text_file_contents(FILE_D2), TWO_GRAM)
+d2_char_2_gram_set = set()
+for n_gram in d2_char_2_grams:
+    d2_char_2_gram_set.add(n_gram)
+print("D2.txt has " + str(len(d2_char_2_gram_set)) + " character 2-grams")
+
+d3_char_2_grams = NGrams.get_character_n_grams(FileIo.get_text_file_contents(FILE_D3), TWO_GRAM)
+d3_char_2_gram_set = set()
+for n_gram in d3_char_2_grams:
+    d3_char_2_gram_set.add(n_gram)
+print("D3.txt has " + str(len(d3_char_2_gram_set)) + " character 2-grams")
+
+d4_char_2_grams = NGrams.get_character_n_grams(FileIo.get_text_file_contents(FILE_D4), TWO_GRAM)
+d4_char_2_gram_set = set()
+for n_gram in d4_char_2_grams:
+    d4_char_2_gram_set.add(n_gram)
+print("D4.txt has " + str(len(d4_char_2_gram_set)) + " character 2-grams")
+
 
 """Construct character 3-grams for all documents"""
-d1_3_grams = NGrams.get_character_n_grams(FileIo.get_text_file_contents(FILE_D1), THREE_GRAM)
-d2_3_grams = NGrams.get_character_n_grams(FileIo.get_text_file_contents(FILE_D2), THREE_GRAM)
-d3_3_grams = NGrams.get_character_n_grams(FileIo.get_text_file_contents(FILE_D3), THREE_GRAM)
-d4_3_grams = NGrams.get_character_n_grams(FileIo.get_text_file_contents(FILE_D4), THREE_GRAM)
+d1_char_3_grams = NGrams.get_character_n_grams(FileIo.get_text_file_contents(FILE_D1), THREE_GRAM)
+d1_char_3_gram_set = set()
+for n_gram in d1_char_3_grams:
+    d1_char_3_gram_set.add(n_gram)
+print("D1.txt has " + str(len(d1_char_3_gram_set)) + " character 3-grams")
 
+d2_char_3_grams = NGrams.get_character_n_grams(FileIo.get_text_file_contents(FILE_D2), THREE_GRAM)
+d2_char_3_gram_set = set()
+for n_gram in d2_char_3_grams:
+    d2_char_3_gram_set.add(n_gram)
+print("D2.txt has " + str(len(d2_char_3_gram_set)) + " character 3-grams")
 
+d3_char_3_grams = NGrams.get_character_n_grams(FileIo.get_text_file_contents(FILE_D3), THREE_GRAM)
+d3_char_3_gram_set = set()
+for n_gram in d3_char_3_grams:
+    d3_char_3_gram_set.add(n_gram)
+print("D3.txt has " + str(len(d3_char_3_gram_set)) + " character 3-grams")
+
+d4_char_3_grams = NGrams.get_character_n_grams(FileIo.get_text_file_contents(FILE_D4), THREE_GRAM)
+d4_char_3_gram_set = set()
+for n_gram in d4_char_3_grams:
+    d4_char_3_gram_set.add(n_gram)
+print("D4.txt has " + str(len(d4_char_3_gram_set)) + " character 3-grams")
+
+"""Construct word 2-grams for all documents"""
+d1_word_2_grams = NGrams.get_word_n_grams(FileIo.get_text_file_contents(FILE_D1), TWO_GRAM)
+d1_word_2_gram_set = set()
+for n_gram in d1_word_2_grams:
+    d1_word_2_gram_set.add(n_gram)
+print("D1.txt has " + str(len(d1_word_2_gram_set)) + " word 2-grams")
+
+d2_word_2_grams = NGrams.get_word_n_grams(FileIo.get_text_file_contents(FILE_D2), TWO_GRAM)
+d2_word_2_gram_set = set()
+for n_gram in d2_word_2_grams:
+    d2_word_2_gram_set.add(n_gram)
+print("D2.txt has " + str(len(d2_word_2_gram_set)) + " word 2-grams")
+
+d3_word_2_grams = NGrams.get_word_n_grams(FileIo.get_text_file_contents(FILE_D3), TWO_GRAM)
+d3_word_2_gram_set = set()
+for n_gram in d3_word_2_grams:
+    d3_word_2_gram_set.add(n_gram)
+print("D3.txt has " + str(len(d3_word_2_gram_set)) + " word 2-grams")
+
+d4_word_2_grams = NGrams.get_word_n_grams(FileIo.get_text_file_contents(FILE_D4), TWO_GRAM)
+d4_word_2_gram_set = set()
+for n_gram in d4_word_2_grams:
+    d4_word_2_gram_set.add(n_gram)
+print("D4.txt has " + str(len(d4_word_2_gram_set)) + " word 2-grams")
--- NGrams.py
+++ NGrams.py
@@ -8,3 +8,13 @@ def get_character_n_grams(file_contents, n):
         file_n_grams = ngrams(file_snippet, n)
         for n_gram in file_n_grams:
             yield(n_gram)
+
+
+"""Return word n-grams"""
+def get_word_n_grams(file_contents, n):
+
+    for file_snippet in file_contents:
+        file_tokens = nltk.word_tokenize(file_snippet)
+        file_n_grams = ngrams(file_tokens, n)
+        for n_gram in file_n_grams:
+            yield (n_gram)
